4aeb3b0a31e1ee518a0c018e3ae488e66cf87ef0
moteo <moteoo@gmail.com>
2018-02-21 00:05:59 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+var x = 1;
+var zeros =0;
+while (Math.pow(5, x) <=number ){
+  zeros = zeros + parseInt(number/Math.pow(5, x));
+  x++;
 }
+  return zeros;// your implementation
+}
+/* I'm not a great matimatician, so the equations for
+this task were found here:
+https://bodheeprep.com/number-exponents-factorial/
+*/
